ac16701df1ae8614d23ae66124ee2c1cecbed47c
世轰。🚀 <yuanshifeng@shihongdeMacBook-Pro.local>
2018-04-01 01:50:10 -0400

    Initial commit

---
+++ src/computersimulatorp1/ComputerSimulatorP1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computersimulatorp1;
+
+/**
+ *
+ * @author 25124
+ */
+public class ComputerSimulatorP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+}
